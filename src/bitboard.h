582df83d6de5a7e53e5e1c2c512872d4ee1c7080
Matthew Helke <matthewhelke52@gmail.com>
2024-11-16 18:07:47 -0600

    add which side to move

--- src/bitboard.h
+++ src/bitboard.h
@@ -12,8 +12,9 @@
 // pop
 #define pop_bit(bitboard, square) (get_bit(bitboard, square) ? bitboard ^= (1ULL << square) : 0)
 
-
 void print_bitboard(U64 bitboard);
+
+// Board representation
 enum {
     a8, b8, c8, d8, e8, f8, g8, h8,
     a7, b7, c7, d7, e7, f7, g7, h7,
@@ -24,4 +25,7 @@ enum {
     a2, b2, c2, d2, e2, f2, g2, h2,
     a1, b1, c1, d1, e1, f1, g1, h1,
 };
+
+// Side to move
+enum { white, black };
 #endif
