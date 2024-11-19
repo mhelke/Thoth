1d39ef3859e14bc9e07240c79a139346482f63a1
Matthew Helke <matthewhelke52@gmail.com>
2024-11-18 19:28:52 -0600

    move random number generator to magics generation

--- src/magics.c
+++ src/magics.c
@@ -3,9 +3,48 @@
 
 #include "bitboard.h"
 #include "attack_tables.h"
-#include "util.h"
 
-// https://www.chessprogramming.org/Looking_for_Magics
+/* 
+This approach attempts to find magic numbers by trying random numbers with a low number of non-zero bits by brute force.
+This code is not run with the engine, but was used to generate the magic numbers seen in [bitboard.c].
+
+Credit for magic generation approach: https://www.chessprogramming.org/Looking_for_Magics#Feeding_in_Randoms
+*/
+
+unsigned int state = 1804289383; // random starting number
+
+unsigned int generate_random_U32_number() {
+    unsigned int num = state;
+
+    // XOR shift 32 algorithm
+    num ^= num << 13;
+    num ^= num >> 17;
+    num ^= num << 5;
+
+    // update state
+    state = num;
+    return num;
+}
+
+ Bitboard generate_random_U64_number() {
+
+    // Random numbers
+    Bitboard n1, n2, n3, n4;
+    
+    // init random numbers slicing 16 bits from MS1B side
+    n1 = (Bitboard)(generate_random_U32_number() & 0xFFFF);
+    n2 = (Bitboard)(generate_random_U32_number() & 0xFFFF);
+    n3 = (Bitboard)(generate_random_U32_number() & 0xFFFF);
+    n4 = (Bitboard)(generate_random_U32_number() & 0xFFFF);
+    
+    // Shuffle
+    return n1 | (n2 << 16) | (n3 << 32) | (n4 << 48);
+ }
+
+ Bitboard generate_magic_bitboard() {
+    return generate_random_U64_number() & generate_random_U64_number() & generate_random_U64_number();
+ }
+
 Bitboard _find_magic_bitboard(int square, int relevant_bits, int is_bishop) {
     Bitboard occupancies[4096];
     Bitboard attacks_table[4096];
--- src/util.h
+++
@@ -1,5 +0,0 @@
-#include "bitboard.h"
-
-unsigned int generate_random_U32_number();
-Bitboard generate_random_U64_number();
-Bitboard generate_magic_bitboard();
