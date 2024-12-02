7cb0e23b2f3920c00ff42658cd2bd07b14022900
Matthew Helke <matthewhelke52@gmail.com>
2024-12-01 18:20:50 -0600

    time util

---
+++ src/util.c
@@ -0,0 +1,15 @@
+#if defined(_WIN32) || defined(_WIN64)
+#include <windows.h>
+#else
+#include <sys/time.h>
+#endif
+
+int get_ms() {
+    #if defined(_WIN32) || defined(_WIN64)
+        return GetTickCount();
+    #else
+        struct timeval tv;
+        gettimeofday(&tv, NULL);
+        return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+    #endif
+}
