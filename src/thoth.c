99fc25497ae5ce9ba9c3cdd157cb482dd5fa819d
Matthew Helke <matthewhelke52@gmail.com>
2025-02-22 17:39:11 -0600

    update debug and test workflows

--- src/thoth.c
+++ src/thoth.c
@@ -18,23 +18,32 @@
 #define test_position "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1 "
 #define mate_position "8/8/8/8/8/K7/Q7/7k w - 0 1 "
 
-int main() {
+int debug_mode() {
+    Board* board = create_board();
+    load_fen(test_position, board);
+    print_board(board);
+    printEval(board);
+    search(10, board);
+    free_board(board);
+    return 0;
+}
+
+int run_tests() {
+    test();
+    perft_tests();
+    return 0;
+}
+
+void initialize() {
     init_hash_keys();
     init_evaluation_masks();
+}
+
+int main() {
+    initialize();
     int debug = 0;
-    if (debug) {
-        Board* board = create_board();
-        load_fen(test_position, board);
-        print_board(board);
-        printEval(board);
-        search(10, board);
-
-        // perft_tests();
-        // test();
-
-        free_board(board);
-
-        return 0;
-    }
+    int tests = 0;
+    if (tests) return run_tests();
+    if (debug) return debug_mode();
     uci_main();
 }
